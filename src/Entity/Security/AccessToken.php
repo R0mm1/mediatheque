b2d76bbaacb24af554a7b62299663b5bc71f52d6
romain2 <romain.quentel.bzh29@gmail.com>
2018-09-19 17:48:58 +0200

    Authentication

---
+++ src/Entity/Security/AccessToken.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: romain2
+ * Date: 15/09/18
+ * Time: 14:51
+ */
+
+namespace App\Entity\Security;
+
+use FOS\OAuthServerBundle\Entity\AccessToken as BaseAccessToken;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="access_tokens", options={"collate"="utf8mb4_unicode_ci", "charset"="utf8mb4"})
+ * @ORM\AttributeOverrides({
+ *     @ORM\AttributeOverride(name="token",
+ *         column=@ORM\Column(
+ *             name   = "token",
+ *             type   = "string",
+ *             length = 128
+ *         )
+ *     )
+ * })
+ */
+class AccessToken extends BaseAccessToken
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Client")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    protected $client;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\User")
+     * @ORM\JoinColumn(name="user_id", referencedColumnName="id", onDelete="CASCADE")
+     */
+    protected $user;
+}
