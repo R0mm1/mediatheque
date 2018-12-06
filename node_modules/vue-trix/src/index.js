b6f813fc5b2f4190f6045c635bd1a620bfc0a6e3
romain2 <romain.quentel.bzh29@gmail.com>
2018-12-06 19:00:36 +0100

    Update npm modules

---
+++ node_modules/vue-trix/src/index.js
@@ -0,0 +1,12 @@
+/*
+* Vue-Trix index.js
+* Author: tranduchanh.ms@gmail.com
+* Github: https://github.com/hanhdt/vue-trix
+*/
+
+import Vue from 'vue'
+import VueTrix from './components/VueTrix.vue'
+
+Vue.component(VueTrix.name, VueTrix)
+
+export default VueTrix
